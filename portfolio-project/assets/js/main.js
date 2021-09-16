7ec528504c0aa3efd8f270df36344cec1139b4fd
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2021-09-16 22:40:08 +1200

    Upload changes

--- portfolio-project/assets/js/main.js
+++ portfolio-project/assets/js/main.js
@@ -93,7 +93,7 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper(".portfolio__container", {
+let swiper = new Swiper(".blog__container", {
     cssMode: true,
     loop: true,
 
